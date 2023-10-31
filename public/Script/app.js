79bc1f4488c8ff6b6c325037c45e30c16ba2eeaa
Munieb009 <amunieb@gmail.com>
2023-10-31 14:22:42 -0400

    A2Update

---
+++ public/Script/app.js
@@ -0,0 +1,8 @@
+// Immidiate invoked function express
+
+(function(){
+    function Start(){
+        console.log("Application started....");
+    }
+    window.addEventListener("load",Start);
+})();
